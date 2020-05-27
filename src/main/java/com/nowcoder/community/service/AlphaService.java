02e0efecdf517595dd2676c1fc40888797b3ac7f
莫言 <sallymy@bu.edu>
2020-05-27 22:49:18 +0800

    test

---
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -0,0 +1,34 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.dao.AlphaDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Service
+//@Scope("prototype")
+public class AlphaService {
+
+    @Autowired
+    private AlphaDao alphaDao;
+
+    public AlphaService(){
+        System.out.println("shilihuaAlphaService");
+    }
+    @PostConstruct
+    public void init(){
+        System.out.println("initializeAlphaService");
+    }
+    @PreDestroy
+    public void destroy(){
+        System.out.println("destroyalphaservice");
+    }
+
+    public String find(){
+        return alphaDao.select();
+    }
+
+}
