776c9e6bc51ad747ea9077c8aa52e419f4fe56cd
莫言 <sallymy@bu.edu>
2020-05-28 20:47:53 +0800

    new

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -3,9 +3,18 @@ package com.nowcoder.community.controller;
 import com.nowcoder.community.service.AlphaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import sun.lwawt.macosx.CPrinterSurfaceData;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/alpha")
@@ -26,4 +35,88 @@ public class AlphaController {
         return alphaService.find();
     }
 
+    @RequestMapping("/http")
+    public void http(HttpServletRequest request, HttpServletResponse response) {
+        //get request
+        System.out.println(request.getMethod());
+        System.out.println(request.getServletPath());
+        Enumeration<String> enumeration = request.getHeaderNames();
+        while(enumeration.hasMoreElements()){
+            String name = enumeration.nextElement();
+            String value = request.getHeader(name);
+            System.out.println(name + ":" + value);
+        }
+        System.out.println(request.getParameter("code"));
+
+        //how to use response return response
+        response.setContentType("text/html:charset=utf-8");
+        try(
+            PrintWriter writer = response.getWriter();
+        ){
+                writer.write("<h1>nowcoder</h1>");
+            }
+            catch(IOException e) {
+                e.printStackTrace();
+            }
+    }
+
+    //GET请求 获取某些数据
+    // /students?current=1&limit=20分页条件
+    @RequestMapping(path = "/students", method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudents(
+            @RequestParam(name = "current", required = false, defaultValue = "1") int current,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) {
+        System.out.println(current);
+        System.out.println(limit);
+        return "some students";
+    }
+    // /student/123
+    @RequestMapping(path = "/student/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudent(@PathVariable("id") int id){
+        System.out.println(id);
+        return "a student";
+    }
+
+    //POST请求
+    @RequestMapping(path = "/student", method = RequestMethod.POST)
+    @ResponseBody
+    public String saveStudent(String name, int age) {
+        System.out.println(name);
+        System.out.println(age);
+        return "success";
+    }
+
+    //响应HTML
+
+    @RequestMapping(path = "/teacher", method = RequestMethod.GET)
+    public ModelAndView getTeacher() {
+        ModelAndView mav= new ModelAndView();
+        mav.addObject("name","张三");
+        mav.addObject("age",30);
+        mav.setViewName("/demo/view");
+        return mav;
+    }
+
+    @RequestMapping(path = "/teacher", method = RequestMethod.GET)
+    public String getSchool(Model model){
+        model.addAttribute("name","北京大学");
+        model.addAttribute("age","80");
+        return "/demo/view";
+
+    }
+
+    //响应json数据(异步请求)
+    //Java对象 JSON字符串 JS对象
+
+    @RequestMapping(path = "/emp", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> getEmp(){
+        Map<String,Object> emp = new HashMap<>();
+        emp.put("name","张三");
+        emp.put("age",23);
+        emp.put("salary", 8000.00);
+        return emp;
+    }
 }
